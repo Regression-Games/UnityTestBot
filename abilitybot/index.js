46fdd2bd4cb3f84325cb666152bb25f4f11aeb0e
abeizer <abby@regression.gg>
2023-07-19 12:35:59 -0400

    log

--- abilitybot/index.js
+++ abilitybot/index.js
@@ -8,6 +8,7 @@ export function configureBot(rg) {
   rg.characterConfig = {
     characterType: CharInfo.type[Math.round(Math.random() * 1000000) % 4]
   };
+  console.log("CONFIGURE BOT WAS CALLED")
 }
 
 export async function processTick(rg) {
